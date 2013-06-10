2f438dad832533a545341dc71e1226eb9729e756
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2013-06-10 17:48:23 +0200

    Repositiory initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShooseStore::Application.config.secret_token = 'd2e32a675e4b99af8a9d1a7b649ad3a74ec93357adfde0189ade981d817563d38d89dabcd3de88ccd20a6eb6176378beacb95671f5ed6d85feac7d5cee2a6753'
